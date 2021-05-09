93a5ebe3e8b5ce61122bc3f047229577ffd57ece
ApurvSinghal <apurv.singhal28@gmail.com>
2021-05-09 13:43:30 +0530

    MongoClient Connection Added

--- ApurvXam/ApurvXam/ViewModels/MainPageViewModel.cs
+++ ApurvXam/ApurvXam/ViewModels/MainPageViewModel.cs
@@ -1,10 +1,12 @@
-﻿using Prism.Commands;
+﻿using MongoDB.Driver;
+using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace ApurvXam.ViewModels
 {
@@ -14,6 +16,20 @@ namespace ApurvXam.ViewModels
             : base(navigationService)
         {
             Title = "Main Page";
+            DbConnection();
         }
+
+        private void DbConnection()
+        {
+            try
+            {
+                var client = new MongoClient("mongodb+srv://DatabaseUser0:Brl6nSbMKXIkwmHX@cluster0.xtwhf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+                var database = client.GetDatabase("test");
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
     }
 }
